ff5a65203743ddd07be1d31ef7430dce848f3d8f
Jie Chen <969149731@qq.com>
2024-01-25 17:14:47 +0800

    v1.4  删除了多余没用的代码

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
- * 启动程序  cj git1111
+ * 启动程序  cj git111112
  * 
  * @author ruoyi
  */
