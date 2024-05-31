fd8d001071beb66eb9d946cbb9849ced55d46130
hhc <939302568@qq.com>
2024-05-31 19:29:27 +0800

    hhc 更改所有非禁用与禁用农药的实现

--- ruoyi-ui/src/views/out/outVegBanPesDetRecords/index.vue
+++ ruoyi-ui/src/views/out/outVegBanPesDetRecords/index.vue
@@ -191,6 +191,14 @@ export default {
           StageName: '运输车超标',
           StageId: 'vehicleEx'
         },
+        {
+          StageName: '其它检出',
+          StageId: 'other'
+        },
+        {
+          StageName: '其它超标',
+          StageId: 'otherEx'
+        },
       ],
       pesticideNameList: [],
       //地区级联
@@ -337,7 +345,7 @@ export default {
       if (rowIndex=== 2)
       {//其中的那一行
         if (columnIndex === 0) {
-          return {rowspan: 6, colspan: 1} // 隐藏表头下面第一行的第一列
+          return {rowspan: 8, colspan: 1} // 隐藏表头下面第一行的第一列
         }
       }
       if(rowIndex> 2){//”其中“包含的行
