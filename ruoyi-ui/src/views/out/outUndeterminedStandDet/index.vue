274a45f900319ac40774688aa2cdeb1d4c42ba5c
Jie Chen <969149731@qq.com>
2024-06-28 09:54:43 +0800

    添加搜索2。 修改人：chenjie

--- ruoyi-ui/src/views/out/outUndeterminedStandDet/index.vue
+++ ruoyi-ui/src/views/out/outUndeterminedStandDet/index.vue
@@ -1,6 +1,57 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+
+
+      <el-form-item label="抽样环节" prop="samplingStageType">
+        <el-input
+          v-model="queryParams.samplingStageType"
+          placeholder="请输入抽样环节"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+
+
+      <el-form-item label="企业名称" prop="enterpriseName">
+        <el-input
+          v-model="queryParams.enterpriseName"
+          placeholder="请输入企业名称/农户"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+
+      <el-form-item label="企业属性" prop="enterpriseAttribute">
+        <el-input
+          v-model="queryParams.enterpriseAttribute"
+          placeholder="请输入企业属性"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+
+      <el-form-item label="信用代码" prop="enterpriseCreditIdCode">
+        <el-input
+          v-model="queryParams.enterpriseCreditIdCode"
+          placeholder="请输入企业信用代码/身份证号"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+
+      <el-form-item label="样品类别" prop="vegFruType">
+        <el-select v-model="queryParams.vegFruType" placeholder="请选择样品类别" clearable>
+          <el-option
+            v-for="dict in dict.type.veg_fru_type"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+
+
       <el-form-item label="检测单位" prop="detectUnit">
         <el-input
           v-model="queryParams.detectUnit"
@@ -260,6 +311,8 @@ import {listOutExceedSampleDetail} from "@/api/out/outExceedSampleDetail";
 
 export default {
   name: "OutUndeterminedStandDet",
+  dicts: ['veg_fru_type'],
+
   data() {
     return {
       samplingAddressProvince: [],//省份集合
@@ -313,6 +366,11 @@ export default {
         samplingProvince: {code:"450000",name:"广西壮族自治区"},
         samplingCity:null,
         samplingTown:null,
+
+        enterpriseName:null,
+        enterpriseAttribute:null,
+        enterpriseCreditIdCode:null,
+        samplingStageType:null,
 
       },
       // 表单参数
